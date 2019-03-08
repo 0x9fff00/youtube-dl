fcbceb98c04e6e5c8cccf536139864c17d619bf1
0x9fff00 <0x9fff00+git@protonmail.ch>
2019-03-08 20:58:12 +0100

    Revert "Merge branch 'urplay-timestamp' into 0x9fff00"
    This reverts commit 6f2015bed6b6de16a894044a819e2987b1b84879, reversing
    changes made to 439a375c43ec68309991b9b0bc52ee72dec3b4fe.

--- youtube_dl/extractor/urplay.py
+++ youtube_dl/extractor/urplay.py
@@ -2,21 +2,18 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
-from ..utils import unified_timestamp
 
 
 class URPlayIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?ur(?:play|skola)\.se/(?:program|Produkter)/(?P<id>[0-9]+)'
     _TESTS = [{
-        'url': 'https://urplay.se/program/209701-framtidens-dromhem-min-oppna-fristad',
-        'md5': 'ec2639a7a0eb3fee4a75dae42f9707ab',
+        'url': 'http://urplay.se/program/190031-tripp-trapp-trad-sovkudde',
+        'md5': 'ad5f0de86f16ca4c8062cd103959a9eb',
         'info_dict': {
-            'id': '209701',
+            'id': '190031',
             'ext': 'mp4',
-            'timestamp': 1547231700,
-            'upload_date': '20190111',
-            'description': 'md5:9663f54d7fc8cc58c27a4c8db550b5d7',
-            'title': 'Framtidens drömhem : Min öppna fristad',
+            'title': 'Tripp, Trapp, Träd : Sovkudde',
+            'description': 'md5:b86bffdae04a7e9379d1d7e5947df1d1',
         },
     }, {
         'url': 'http://urskola.se/Produkter/155794-Smasagor-meankieli-Grodan-i-vida-varlden',
@@ -54,7 +51,6 @@ class URPlayIE(InfoExtractor):
             'title': urplayer_data['title'],
             'description': self._og_search_description(webpage),
             'thumbnail': urplayer_data.get('image'),
-            'timestamp': unified_timestamp(self._html_search_meta('uploadDate', webpage)),
             'series': urplayer_data.get('series_title'),
             'subtitles': subtitles,
             'formats': formats,
